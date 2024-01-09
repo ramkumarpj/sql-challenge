5a0f415cfee7264ee38bf1afb0c99c3b893b4aab
Ramkumar Jothis <611317+ramkumarpj@users.noreply.github.com>
2024-01-08 21:23:20 -0600

    Update EmployeeDB_Analysis.sql

--- EmployeeSQL/EmployeeDB_Analysis.sql
+++ EmployeeSQL/EmployeeDB_Analysis.sql
@@ -1,6 +1,6 @@
 --List the employee number, last name, first name, sex, and salary of each employee.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM "Employees" AS e join "Salaries" AS s 
+FROM "Employees" AS e left join "Salaries" AS s 
 ON e.emp_no = s.emp_no;
 
 --List the first name, last name, and hire date for the employees who were hired in 1986.
